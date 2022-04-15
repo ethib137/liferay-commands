67ed61e4d267820898c4f9fe4a73946f5c788522
Mahmoud Hussein Tayem <77802068+mahmoudhtayem87@users.noreply.github.com>
2022-04-15 06:15:30 +0400

    Initial commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const program = require('commander');
 const lrupdate = require('./commands/lrupdate');
-
+const clearcache = require('./commands/clearcache');
 program
     .command('update')
     .alias('u')
@@ -12,6 +12,15 @@ program
     // function to execute when command is uses
     .action(function (update, args) {
         lrupdate.start(update.old,update.new);
+    });
+program
+    .command('clear-cache')
+    .alias('r')
+    .description('Update liferay 7.4 bundle by moving the required modules and files from the old bundle to the new bundle.') // command description
+    .option('-s, --bundle [value]', 'Liferay 7.4 old bundle')
+    // function to execute when command is uses
+    .action(function (clear, args) {
+        clearcache.clear(clear.bundle);
     });
 // allow commander to parse `process.argv`
 program.parse(process.argv);
