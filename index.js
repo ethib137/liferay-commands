afc9c881d586023fac85790149cc336cb7a6ad67
Evan Thibodeau <evan.thibodeau@liferay.com>
2022-04-15 15:01:45 +0400

    Prettier Format

--- index.js
+++ index.js
@@ -4,23 +4,27 @@ const program = require('commander');
 const lrupdate = require('./commands/lrupdate');
 const clearcache = require('./commands/clearcache');
 program
-    .command('update')
-    .alias('u')
-    .description('Update liferay 7.4 bundle by moving the required modules and files from the old bundle to the new bundle.') // command description
-    .option('-s, --old [value]', 'Liferay 7.4 old bundle')
-    .option('-s, --new [value]', 'Liferay 7.4 new bundle')
-    // function to execute when command is uses
-    .action(function (update, args) {
-        lrupdate.start(update.old,update.new);
-    });
+	.command('update')
+	.alias('u')
+	.description(
+		'Update liferay 7.4 bundle by moving the required modules and files from the old bundle to the new bundle.'
+	) // command description
+	.option('-s, --old [value]', 'Liferay 7.4 old bundle')
+	.option('-s, --new [value]', 'Liferay 7.4 new bundle')
+	// function to execute when command is uses
+	.action(function (update, args) {
+		lrupdate.start(update.old, update.new);
+	});
 program
-    .command('clear-cache')
-    .alias('r')
-    .description('Update liferay 7.4 bundle by moving the required modules and files from the old bundle to the new bundle.') // command description
-    .option('-s, --bundle [value]', 'Liferay 7.4 old bundle')
-    // function to execute when command is uses
-    .action(function (clear, args) {
-        clearcache.clear(clear.bundle);
-    });
+	.command('clear-cache')
+	.alias('r')
+	.description(
+		'Update liferay 7.4 bundle by moving the required modules and files from the old bundle to the new bundle.'
+	) // command description
+	.option('-s, --bundle [value]', 'Liferay 7.4 old bundle')
+	// function to execute when command is uses
+	.action(function (clear, args) {
+		clearcache.clear(clear.bundle);
+	});
 // allow commander to parse `process.argv`
 program.parse(process.argv);
